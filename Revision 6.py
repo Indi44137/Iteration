ad0eab8b93f19e9d5f3cd97a5d3241c187ea46da
Indi Knighton <44137@longroad.ac.uk>
2014-10-21 14:56:42 +0100

    Revision task 6

---
+++ Revision 6.py
@@ -0,0 +1,17 @@
+#Indi Knighton
+#21/10/2014
+#Program converting Kg into pounds
+
+number = 0
+times_table = 2.2
+result = number * times_table
+while number < 21:
+    print("{0:>2} Kg X {1:>2} = {2:>2.2f} pounds".format(number, times_table, result))
+    number = number + 1
+    result = number * times_table
+           
+    
+    
+    
+        
+  
